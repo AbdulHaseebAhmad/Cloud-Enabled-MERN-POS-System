1eae48dcc57fdfe90a461180d512ba4e10d49403
Abdul Haseeb <abdull.haseebkhan@gmail.com>
2025-01-11 17:45:45 +0000

    Socket Variable

--- Frontend/src/utilities/Socket-Connection.js
+++ Frontend/src/utilities/Socket-Connection.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io(import.meta.env.VITE_APP_BACKEND_API_URL, {
   autoConnect: false,
   autoDisconnect: false,
   reconnection: true,
